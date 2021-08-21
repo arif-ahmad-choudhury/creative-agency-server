bf6cc4f83772117f6a0b366b5b5d6dd8e6e71034
Arifac83 <60567004+Arifac83@users.noreply.github.com>
2021-08-21 22:10:32 +0600

    server completed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(cors());
 app.use(express.static('service'));
 app.use(fileUpload());
